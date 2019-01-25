b9b647c1e88facfc806603275b8fe0a0c3c9a0c5
Lovizu <nug22kr@gmail.com>
2019-01-25 17:14:13 +0900

    server item

--- app/Http/Controllers/MedicalController.php
+++ app/Http/Controllers/MedicalController.php
@@ -9,9 +9,18 @@ class MedicalController extends Controller
 {
     public function medical($type, $lat, $lng, $km = 1)
     {
-        return Medical::Distance($lat, $lng, $km)
-            ->where('type', $type)
-            ->get();
+        $medicals = Medical::Distance($lat, $lng, $km)
+            ->where('type', $type);
+
+        if ('hospital' == $type) {
+            $medicals->where(function ($q) {
+                $q->where('name', 'like', '%소아%')
+                ->orWhere('etc', 'like', '%영유아%')
+                ->orWhere('etc', 'like', '%어린이%');
+            });
+        }
+
+        return $medicals->get();
     }
 
     public function kidz($lat, $lng, $km = 1)
